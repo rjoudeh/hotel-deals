a1653e64b1f1efc73ed1449abfde088e4a201359
Rania Joudeh <rania@reserveout.com>
2017-08-08 12:48:18 +0300

    add google analytics

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,4 +1,20 @@
 $(function() {
+
+	(function(i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function() {
+			(i[r].q = i[r].q || []).push(arguments)
+		}, i[r].l = 1 * new Date();
+		a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+		a.async = 1;
+		a.src = g;
+		m.parentNode.insertBefore(a, m)
+	})(window, document, 'script',
+			'https://www.google-analytics.com/analytics.js', 'ga');
+
+	ga('create', 'UA-104236411-1', 'auto');
+	ga('send', 'pageview');
+
 	$(".rateYo").each(function() {
 		var rate = $(this).attr('rate');
 
@@ -27,20 +43,21 @@ $(function() {
 	if ($("#toRateInput").val() != '')
 		$("#toRate").rateYo("option", "rating", $("#toRateInput").val());
 
-	$(".clear").bind(
-			"click",
-			function() {
-				$(this).closest('form').find(
-						"input[type=text], input[type=date], input[type=number]").val("");
+	$(".clear")
+			.bind(
+					"click",
+					function() {
+						$(this)
+								.closest('form')
+								.find(
+										"input[type=text], input[type=date], input[type=number]")
+								.val("");
 
-				$("#fromRate").rateYo("option", "rating",
-						0);
-				$("#toRate")
-				.rateYo("option", "rating", 0);
-				
-				
-				$("#fromRateInput").val('');
-				$("#toRateInput").val('');
+						$("#fromRate").rateYo("option", "rating", 0);
+						$("#toRate").rateYo("option", "rating", 0);
 
-			});
+						$("#fromRateInput").val('');
+						$("#toRateInput").val('');
+
+					});
 });
